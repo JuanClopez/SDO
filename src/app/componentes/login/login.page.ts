f4e8f3b44b12d24f748ac287c1f773b678552953
Juan Camilo López <39039753+JuanClopez@users.noreply.github.com>
2019-07-29 15:25:57 -0500

    Errores de Auth
    AngularFireAuth falta en app.module.ts y   loging.page.ts

--- src/app/componentes/login/login.page.ts
+++ src/app/componentes/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../servicios/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onSubmitLogin()
+  {
+    console.log('estas en la funcion')
+
   }
 
 }
