e69fe648e1d6947254b7fa6956ab535e6b83a1bc
Juan Camilo López <39039753+JuanClopez@users.noreply.github.com>
2019-07-29 13:08:50 -0500

    Configuracion Firebase ok
    configuracion de la conexion hasta la parte donde falla el modulo en import
    AngularFireAuth en app.module.ts min 21:12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,15 @@
 export const environment = {
   production: false
 };
+export const firebaseConfig = {
+  apiKey: "AIzaSyC8tSQQegH8WNvTmj81mHY4uDdxFDe654s",
+  authDomain: "sdopticas-app.firebaseapp.com",
+  databaseURL: "https://sdopticas-app.firebaseio.com",
+  projectId: "sdopticas-app",
+  storageBucket: "",
+  messagingSenderId: "123520537190",
+  appId: "1:123520537190:web:a0f18a74126fe813"
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
